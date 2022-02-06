6324301b0f7beaa4b10831bde75893e08af8a85c
charmilien <72658425+charmilien@users.noreply.github.com>
2022-02-06 19:38:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+  export const firebaseConfig = {
+    apiKey: "AIzaSyAz4eUIRl7xaGv3aVes8za_2VIFbVi_JGw",
+    authDomain: "gettry1-813ba.firebaseapp.com",
+    databaseURL: "https://gettry1-813ba-default-rtdb.firebaseio.com",
+    projectId: "gettry1-813ba",
+    storageBucket: "gettry1-813ba.appspot.com",
+    messagingSenderId: "151689395530",
+    appId: "1:151689395530:web:5947f193d01fd9a38fd5b3"
+  };
+  
